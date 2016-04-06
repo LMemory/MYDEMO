7781d0724d916425560e6ef5fc69aa80aa1b34aa
hongliang <https://git.oschina.net/myyibanm/gittest.git>
2016-04-06 17:49:30 +0800

    第二次修改

--- app/src/main/java/hongliang/com/easy/MainActivity.java
+++ app/src/main/java/hongliang/com/easy/MainActivity.java
@@ -2,13 +2,52 @@ package hongliang.com.easy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout vertcalLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+//        System.out.println("List<String>转化示例开始----------");
+//        List<String> list = new ArrayList<String>();
+//        list.add("fastjson1");
+//        list.add("fastjson2");
+//        list.add("fastjson3");
+//        String jsonString = JSON.toJSONString(list);
+//        System.out.println("json字符串:" + jsonString);
+//
+//        //解析json字符串
+//        List<String> list2 = JSON.parseObject(jsonString, new TypeReference<List<String>>() {
+//        });
+//        System.out.println(list2.get(0) + "::" + list2.get(1) + "::" + list2.get(2));
+//        System.out.println("List<String>转化示例结束----------");
+
+
+        vertcalLayout1 = (LinearLayout) findViewById(R.id.vertcalLayout1);
+        Button b = new Button(this);
+        b.setText("btn1");
+        b.setOnClickListener(this);
+        b.setPadding(10, 10, 10, 10);
+        vertcalLayout1.addView(b);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == vertcalLayout1.getChildAt(0)) {
+
+        }
     }
 }
---
+++ app/src/main/java/hongliang/com/easy/TwoActivity.java
@@ -0,0 +1,29 @@
+package hongliang.com.easy;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        findViewById(R.id.doid).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "FAB", Snackbar.LENGTH_LONG)
+                        .setAction("cancel", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                //这里的单击事件代表点击消除Action后的响应事件
+                            }
+                        })
+                        .show();
+
+            }
+        });
+    }
+}
